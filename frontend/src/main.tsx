d3c799808eedf7b34696db8349138833c9daff18
Dugd_Nitkin <dugd.nitkin@gmail.com>
2025-11-12 10:01:07 +0200

    fix: fix routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Toaster } from 'sonner';
-import PrivateRoute from './components/deprecated/PrivateRoute';
 import './index.css';
 import Chats from './pages/Chats';
 import ChatView from './pages/ChatView';
@@ -38,7 +37,6 @@ createRoot(document.getElementById('root')!).render(
             <Route path="/chats/:id" element={<ChatView/>}/>
             <Route path="/posts" element={<Posts/>}/>
             <Route path="/profile/:username" element={<Profile/>}/>
-            <Route path="/profile" element={<PrivateRoute><Profile/></PrivateRoute>}/>
           </Routes>
         </BrowserRouter>
       </AuthProvider>
